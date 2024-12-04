e327b0cebaf56c2d929b06de207b99c0e215dc04
Trued Coinventor <trued.coinventor+portefeuille@proton.me>
2024-12-04 15:55:18 +0100

    created basic project

---
+++ src/main/java/dev/portefeuille/portefeuilleback/PortefeuilleBackApplication.java
@@ -0,0 +1,13 @@
+package dev.portefeuille.portefeuilleback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortefeuilleBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortefeuilleBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/portefeuille/portefeuilleback/PortefeuilleBackApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.portefeuille.portefeuilleback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortefeuilleBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
